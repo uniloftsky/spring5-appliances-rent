2fd0a0b55ba715167a6322bfc871c3cf7f3992ff
uniloftsky <uniloftsky@gmail.com>
2021-01-14 01:04:42 +0200

    project added

---
+++ src/main/java/com/uniloftsky/springframework/spring5appliancesrent/AppliancesRentApplication.java
@@ -0,0 +1,13 @@
+package com.uniloftsky.springframework.spring5appliancesrent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppliancesRentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppliancesRentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uniloftsky/springframework/spring5appliancesrent/AppliancesRentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniloftsky.springframework.spring5appliancesrent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppliancesRentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
