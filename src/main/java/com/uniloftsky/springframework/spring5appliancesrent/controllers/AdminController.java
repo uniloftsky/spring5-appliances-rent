e3f92c26d9aadf2295f5b78d2d679f5d47a011cd
uniloftsky <uniloftsky@gmail.com>
2021-03-11 18:37:29 +0200

    implemented back-end on admin-panel. closes #10

--- src/main/java/com/uniloftsky/springframework/spring5appliancesrent/controllers/AdminController.java
+++ src/main/java/com/uniloftsky/springframework/spring5appliancesrent/controllers/AdminController.java
@@ -3,16 +3,13 @@ package com.uniloftsky.springframework.spring5appliancesrent.controllers;
 import com.uniloftsky.springframework.spring5appliancesrent.comparators.item.ItemAscComparatorById;
 import com.uniloftsky.springframework.spring5appliancesrent.comparators.renting.RentingAscComparatorById;
 import com.uniloftsky.springframework.spring5appliancesrent.comparators.user.UserAscComparatorById;
-import com.uniloftsky.springframework.spring5appliancesrent.model.Item;
-import com.uniloftsky.springframework.spring5appliancesrent.model.Renting;
-import com.uniloftsky.springframework.spring5appliancesrent.model.User;
-import com.uniloftsky.springframework.spring5appliancesrent.services.ItemService;
-import com.uniloftsky.springframework.spring5appliancesrent.services.RentingService;
-import com.uniloftsky.springframework.spring5appliancesrent.services.UserService;
+import com.uniloftsky.springframework.spring5appliancesrent.model.*;
+import com.uniloftsky.springframework.spring5appliancesrent.services.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Comparator;
@@ -24,18 +21,24 @@ public class AdminController {
     private final ItemService itemService;
     private final RentingService rentingService;
     private final UserService userService;
+    private final TypeService typeService;
+    private final CategoryService categoryService;
     private final Comparator<Item> itemComparatorAscById = new ItemAscComparatorById();
     private final Comparator<Renting> rentingComparatorAscById = new RentingAscComparatorById();
     private final Comparator<User> userComparatorAscById = new UserAscComparatorById();
 
-    public AdminController(ItemService itemService, RentingService rentingService, UserService userService) {
+    public AdminController(ItemService itemService, RentingService rentingService, UserService userService, TypeService typeService, CategoryService categoryService) {
         this.itemService = itemService;
         this.rentingService = rentingService;
         this.userService = userService;
+        this.typeService = typeService;
+        this.categoryService = categoryService;
     }
 
     @GetMapping("/admin")
-    public String getAdminPanel() {
+    public String getAdminPanel(Model model) {
+        model.addAttribute("type", new Type());
+        model.addAttribute("category", new Category());
         return "admin/admin";
     }
 
@@ -49,6 +52,8 @@ public class AdminController {
     public String getUserDetail(@RequestParam("id") Long id, Model model) {
         model.addAttribute("user", userService.findById(id));
         model.addAttribute("userItems", userService.getUserItems(userService.findById(id), itemComparatorAscById));
+        model.addAttribute("type", new Type());
+        model.addAttribute("category", new Category());
         return "admin/userDetail";
     }
 
@@ -63,6 +68,19 @@ public class AdminController {
         userService.delete(userService.findById(id));
         return "redirect:/admin";
     }
+
+    @PostMapping("/typeAdd")
+    public String typeAdd(@ModelAttribute("type") Type type) {
+        typeService.save(type);
+        return "redirect:/admin";
+    }
+
+    @PostMapping("/categoryAdd")
+    public String categoryAdd(@ModelAttribute("category") Category category) {
+        categoryService.save(category);
+        return "redirect:/admin";
+    }
+
 
     @ModelAttribute("posts")
     public Set<Item> getPosts() {
